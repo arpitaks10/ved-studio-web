ecb7b1f1d57ad9ab05ae37bb493877a094e7639c
dmachine7 <60094449+dmachine7@users.noreply.github.com>
2020-11-26 00:10:48 +0530

    initial-commit

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import Hero from '../Components/Hero/Hero';
+import DemoVideo from '../Components/DemoVideo/DemoVideo';
+import ServiceTier from '../Components/ServiceTier/ServiceTier';
+import './Home.css';
 
 const Home = () => {
-    return (
-        <div>
-            he
-        </div>
-    );
+  const homeSections = [
+    {
+      sectionName : "Demo Video",
+      sectionDesc : "A demo video to help you get familiar with ved.studio",
+      sectionContent : <DemoVideo />
+    },
+    {
+      sectionName : "Service Tier",
+      sectionDesc : "Select the service based on your organization size and need",
+      sectionContent : <ServiceTier />
+    }
+  ];
+
+  return (
+    <div className = "app-sections">
+      <Hero />
+      {
+        homeSections.map((item) => {
+          return (   
+            <div className = "section">         
+              <div className = "section-head"> {item.sectionName} </div>
+              <div className = "section-desc"> {item.sectionDesc} </div>
+              <div className = "section-content"> {item.sectionContent} </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 };
 
 export default Home;
